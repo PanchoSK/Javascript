4174a53d623519dd1fc51db90885124098ce7ec9
Vlatko <krstevski.vlatko@gmail.com>
2024-12-03 23:04:27 +0100

    Add files via upload

---
+++ Domasna 2.1 (Updated)/script/script.js
@@ -0,0 +1,29 @@
+// homework 2.1.2
+let numberOfMinutes = 5;
+let convertRate = 60;
+
+let totalSeconds = numberOfMinutes*convertRate;
+console.log(`${numberOfMinutes} minutes are equal to ${totalSeconds} seconds`);
+
+//homework 2.1.3
+let aNummber=21;
+aNummber++;
+console.log(`New value of this number is increament by 1 and now is ${aNummber}`);
+
+//homework 2.1.4
+let triangleHeight = 5;
+let triangleBase = 4;
+let areaFormula = (triangleHeight*triangleBase)/2;
+console.log(`Area of triangle with ${triangleHeight}cm height and ${4}cm base is ${areaFormula}cm2`);
+
+//homework 2.1.5
+let lenghtValue = 5;
+let widthValue = 3;
+let perimeterFormmula = (lenghtValue+widthValue)*2;
+console.log(`Perimeter of rectangle with ${lenghtValue}cm lenght and ${widthValue}cm width is ${perimeterFormmula}cm`);
+
+//homework 2.1.6
+let circleRadius = 5;
+let pi = 3.14;
+let circleAreaFormula = pi*(circleRadius*circleRadius);
+console.log(`Area of circle is ${circleAreaFormula}cm2`);
